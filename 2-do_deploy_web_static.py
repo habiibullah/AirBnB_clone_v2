d7412c7442b57f9ce606bc4e1661db57eae44b2e
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-06 23:16:48 +0000

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,39 @@
+
 #!/usr/bin/python3
+"""Compress web static package
 """
-Script generates a .tgz archive from web_static folder
-"""
-from fabric.operations import local, run, put, env
+from fabric.api import *
 from datetime import datetime
-import os
+from os import path
 
 
 env.hosts = ['35.168.2.117', '54.146.9.154']
-env.user = 'ubuntu'
+#env.user = 'ubuntu'
+
 
 
 def do_deploy(archive_path):
+    """Deploy web files to server
     """
-        Distributes an archive to your web servers
-    """
-    if not exists(archive_path):
-        return False
-
-    _path = archive_path.split("/")
-    path_no_ext = _path[1].split(".")[0]
-
     try:
-        put(archive_path, "/tmp")
-        run("sudo mkdir -p /data/web_static/releases/" + path_no_ext + "/")
-        run("sudo tar -xzf /tmp/" + path_no_ext + ".tgz" +
-            " -C /data/web_static/releases/" + path_no_ext + "/")
-        run("sudo rm /tmp/" + path_no_ext + ".tgz")
-        run("sudo mv /data/web_static/releases/" + path_no_ext +
-            "/web_static/* /data/web_static/releases/" + path_no_ext + "/")
-        run("sudo rm -rf /data/web_static/releases/" +
-            path_no_ext + "/web_static")
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/" + path_no_ext +
-            "/ /data/web_static/current")
-        return True
-
-    except Exception:
+        if not (path.exists(archive_path)):
+            return False
+        put(archive_path, '/tmp/')
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
+        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+        run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'
+            .format(timestamp))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(timestamp))
+    except FileNotFoundError:
         return False
+    return True
